37c5923a7eebf14c2fdaf67f506e97efeccb11b2
Akira Yanagihara <sin@sinack.com>
2019-12-11 04:50:47 +0900

    change fragment

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,13 @@ import MessagesList from "./MessagesList";
 import Menu from "./Menu";
 
 const routes = (
-  <React.Fragment>
+  <>
     <Menu />
     <Switch>
       <Route exact path="/" component={Counter} />
       <Route path="/messages" component={MessagesList} />
     </Switch>
-  </React.Fragment>
+  </>
 );
 
 export default routes;
